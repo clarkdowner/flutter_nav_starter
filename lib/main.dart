7365ac38df18196429b7a190d3bc1ed46c8cd481
Clark Downer <cddowner@gmail.com>
2018-09-01 13:57:50 -0400

    Initial BottomNavigationBar construction

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: new ThemeData(primarySwatch: Colors.blue,),
+      home: new Home(title: 'Flutter Starter Project'),
     );
   }
 }
