717651d8d13390a269c249df6d4670ca96393d55
Clark Downer <cddowner@gmail.com>
2018-09-02 20:38:54 -0700

    Move widgets to single file and add additional bottom navigations

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'placeholder_widget.dart';
-import 'drawer.dart';
+//import 'placeholder_widget.dart';
+//import 'drawer.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
@@ -15,9 +15,11 @@ class _HomeState extends State<Home> {
 
   int _currentIndex = 0;
   final List<Widget> _children = [
-    PlaceholderWidget(Colors.white),
-    PlaceholderWidget(Colors.red),
-    PlaceholderWidget(Colors.green),
+    HomeScreen(),
+    CalendarScreen(),
+    StatsScreen(),
+    DiscussionScreen(),
+    FlaggedScreen(),
   ];
 
   void onTabTapped(int index) {
@@ -52,12 +54,109 @@ class _HomeState extends State<Home> {
             title: new Text('Home'),
           ),
           BottomNavigationBarItem(
-            icon: new Icon(Icons.ac_unit),
+            icon: new Icon(Icons.calendar_today),
             title: new Text('Placeholder'),
           ),
           BottomNavigationBarItem(
-            icon: new Icon(Icons.satellite),
+            icon: new Icon(Icons.equalizer),
             title: new Text('Placeholder'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.forum),
+            title: new Text('Placeholder'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.outlined_flag),
+            title: new Text('Placeholder'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlaceholderWidget(Colors.white);
+  }
+}
+
+
+class CalendarScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlaceholderWidget(Colors.amber);
+  }
+}
+
+
+class StatsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlaceholderWidget(Colors.green);
+  }
+}
+
+
+class DiscussionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlaceholderWidget(Colors.purple);
+  }
+}
+
+
+class FlaggedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlaceholderWidget(Colors.lightGreen);
+  }
+}
+
+
+class PlaceholderWidget extends StatelessWidget {
+  final Color color;
+
+  PlaceholderWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+    );
+  }
+}
+
+
+class DrawerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero, // Remove any padding from the ListView
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Temp Header'),
+            // TODO: inherit from context widget
+            decoration: BoxDecoration(
+              color: Colors.amberAccent,
+            ),
+          ),
+          ListTile(
+            title: Text('First Title'),
+            onTap: () {
+              // TODO: probably should have a universal drawer navigation function
+              // function should update state of app and then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Second Title'),
+            onTap: () {
+              Navigator.pop(context);
+            },
           ),
         ],
       ),
--- lib/placeholder_widget.dart
+++ lib/placeholder_widget.dart
@@ -1,14 +1,3 @@
 import 'package:flutter/material.dart';
 
-class PlaceholderWidget extends StatelessWidget {
-  final Color color;
 
-  PlaceholderWidget(this.color);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: color,
-    );
-  }
-}
